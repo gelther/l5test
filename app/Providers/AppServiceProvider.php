d29d81e5f2397d4936a40b17d69f4af4767f80b9
gelther <lpw.sormano@gmail.com>
2016-04-16 11:41:13 +0200

    Code consistency changes for app/Providers/AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,7 +9,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      *
-     * @return void
+     * @return  void
      */
     public function boot()
     {
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      *
-     * @return void
+     * @return  void
      */
     public function register()
     {
